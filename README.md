d34852db46cae607a9202a7f254d2fb8c161f288
yangCJ <yangchaojie456@126.com>
2019-06-28 00:32:43 +0800

    0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# just-preview
+# just-preview README
+
+
+
+[![图片](https://yang-cj.oss-cn-shanghai.aliyuncs.com/111.gif?Expires=1561653422&OSSAccessKeyId=TMP.AgEpwVaIWgTE1iX_h3EZpfPA7Q6R7u4qO-bR3P5_qVipSdYPk9mFYd9MTeueADAtAhQozSRu4f-FcOlpTDX7DDePMQiRSAIVAI45ly8i68tZJp3o2NV7rzn85kHc&Signature=XKz0r1PrEKY2Q3JkgJh4y0t0C1w%3D)](https://yang-cj.oss-cn-shanghai.aliyuncs.com/111.gif?Expires=1561653422&OSSAccessKeyId=TMP.AgEpwVaIWgTE1iX_h3EZpfPA7Q6R7u4qO-bR3P5_qVipSdYPk9mFYd9MTeueADAtAhQozSRu4f-FcOlpTDX7DDePMQiRSAIVAI45ly8i68tZJp3o2NV7rzn85kHc&Signature=XKz0r1PrEKY2Q3JkgJh4y0t0C1w%3D)
+
+
+
+## Features
+
+Add preview in vscode to avoid switching back and forth between browsers.
+
+Fast page layout.
+
+在vscode中加入预览界面，避免来回切换浏览器。
+快速排版页面。
+
+## Known Issues
+
+If the page is out of screen height, close the no scroll TAB
+
+如果页面超出屏幕的高度，请关闭 no scroll选项卡
+
+
+**Enjoy!**
+https://github.com/yangchaojie456/just-preview
---
+++ extension.js
@@ -0,0 +1,68 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "just-preview" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('extension.JustPreview', function () {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		// vscode.window.showInformationMessage('Hello World!');
+
+		// 创建webview
+		const panel = vscode.window.createWebviewPanel(
+			'testWebview', // viewType
+			"just preview", // 视图标题
+			vscode.ViewColumn.One, // 显示在编辑器的哪个部位
+			{
+				enableScripts: true, // 启用JS，默认禁用
+				retainContextWhenHidden: true, // webview被隐藏时保持状态，避免被重置
+			}
+		);
+		panel.webview.html = getWebViewContent(context, './index.html');
+	});
+
+	context.subscriptions.push(disposable);
+
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
+
+/**
+ * 从某个HTML文件读取能被Webview加载的HTML内容
+ * @param {*} context 上下文
+ * @param {*} templatePath 相对于插件根目录的html文件相对路径
+ */
+function getWebViewContent(context, templatePath) {
+    const resourcePath = path.join(context.extensionPath, templatePath);
+    const dirPath = path.dirname(resourcePath);
+    let html = fs.readFileSync(resourcePath, 'utf-8');
+    // vscode不支持直接加载本地资源，需要替换成其专有路径格式，这里只是简单的将样式和JS的路径替换
+    html = html.replace(/(<link.+?href="|<script.+?src="|<img.+?src=")(.+?)"/g, (m, $1, $2) => {
+        return $1 + vscode.Uri.file(path.resolve(dirPath, $2)).with({ scheme: 'vscode-resource' }).toString() + '"';
+    });
+    return html;
+}
